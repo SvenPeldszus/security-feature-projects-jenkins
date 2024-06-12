525fd5448658b2c66e97af9192f7a38c344df569
yusudo21 <>
2024-06-12 22:49:11 +0200

    BasicHeaderApiTokenAuthenticator annotiert

--- core/src/main/java/jenkins/security/BasicHeaderApiTokenAuthenticator.java
+++ core/src/main/java/jenkins/security/BasicHeaderApiTokenAuthenticator.java
@@ -2,17 +2,21 @@ package jenkins.security;
 
 import static java.util.logging.Level.WARNING;
 
-import hudson.Extension;
-import hudson.model.User;
 import java.util.logging.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.gravity.security.annotations.requirements.Secrecy;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import hudson.Extension;
+import hudson.model.User;
 
 /**
  * Checks if the password given in the BASIC header matches the user's API token.
@@ -24,10 +28,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @Extension
 public class BasicHeaderApiTokenAuthenticator extends BasicHeaderAuthenticator {
     /**
-     * Note: if the token does not exist or does not match, we do not use {@link SecurityListener#fireFailedToAuthenticate(String)}
-     * because it will be done in the {@link BasicHeaderRealPasswordAuthenticator} in the case the password is not valid either
+     * Note: if the token does not exist or does not match, we do not use
+     * {@link SecurityListener#fireFailedToAuthenticate(String)} because it will be done in the
+     * {@link BasicHeaderRealPasswordAuthenticator} in the case the password is not valid either
      */
     @Override
+    @Secrecy
+    // Parameter enthaelt req, username und password
     public Authentication authenticate2(HttpServletRequest req, HttpServletResponse rsp, String username, String password) throws ServletException {
         User u = BasicApiTokenHelper.isConnectingUsingApiToken(username, password);
         if (u != null) {
@@ -38,8 +45,10 @@ public class BasicHeaderApiTokenAuthenticator extends BasicHeaderAuthenticator {
 
                 SecurityListener.fireAuthenticated2(userDetails);
             } catch (UsernameNotFoundException x) {
-                // The token was valid, but the impersonation failed. This token is clearly not his real password,
-                // so there's no point in continuing the request processing. Report this error and abort.
+                // The token was valid, but the impersonation failed. This token is clearly not his
+                // real password,
+                // so there's no point in continuing the request processing. Report this error and
+                // abort.
                 LOGGER.log(WARNING, "API token matched for user " + username + " but the impersonation failed", x);
                 throw new ServletException(x);
             }
