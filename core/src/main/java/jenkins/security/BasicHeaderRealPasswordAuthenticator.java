06bfbf1599160262330d7569696a61faa9b39ecf
yusudo21 <>
2024-06-12 22:40:29 +0200

    BasicHeaderRealPasswordAuthenticator annotiert

--- core/src/main/java/jenkins/security/BasicHeaderRealPasswordAuthenticator.java
+++ core/src/main/java/jenkins/security/BasicHeaderRealPasswordAuthenticator.java
@@ -17,16 +17,16 @@ package jenkins.security;
 
 import static java.util.logging.Level.FINER;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import hudson.Extension;
 import java.io.IOException;
 import java.util.logging.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import jenkins.ExtensionFilter;
-import jenkins.model.Jenkins;
-import jenkins.util.SystemProperties;
+
+import org.gravity.security.annotations.requirements.Critical;
+import org.gravity.security.annotations.requirements.Integrity;
+import org.gravity.security.annotations.requirements.Secrecy;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
@@ -35,35 +35,49 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import hudson.Extension;
+import jenkins.ExtensionFilter;
+import jenkins.model.Jenkins;
+import jenkins.util.SystemProperties;
+
 /**
- * Checks if the password given in the BASIC header matches the user's actual password,
- * as opposed to other pseudo-passwords like API tokens.
+ * Checks if the password given in the BASIC header matches the user's actual password, as opposed
+ * to other pseudo-passwords like API tokens.
  *
  * @author Kohsuke Kawaguchi
  * @since 1.576
  */
 @Restricted(NoExternalUse.class)
 @Extension
+@Critical(secrecy = { "BasicHeaderRealPasswordAuthenticator.authenticationDetailsSource:AuthenticationDetailsSource" }, integrity = { "authenticationDetailsSource:AuthenticationDetailsSource" })
 public class BasicHeaderRealPasswordAuthenticator extends BasicHeaderAuthenticator {
+    @Secrecy
+    @Integrity
     private AuthenticationDetailsSource authenticationDetailsSource = new WebAuthenticationDetailsSource();
 
     @Override
     public Authentication authenticate2(HttpServletRequest req, HttpServletResponse rsp, String username, String password) throws IOException, ServletException {
-        if (DISABLE)
+        if (DISABLE) {
             return null;
+        }
 
         UsernamePasswordAuthenticationToken authRequest =
                 new UsernamePasswordAuthenticationToken(username, password);
+        // &begin[use_authDetailsSource]
         authRequest.setDetails(authenticationDetailsSource.buildDetails(req));
+        // &end[use_authDetailsSource]
 
         try {
+            // &begin[use_authDetailsSource]
             Authentication a = Jenkins.get().getSecurityRealm().getSecurityComponents().manager2.authenticate(authRequest);
+            // &end[use_authDetailsSource]
             // Authentication success
             LOGGER.log(FINER, "Authentication success: {0}", a);
             return a;
         } catch (AuthenticationException failed) {
             // Authentication failed
-            LOGGER.log(FINER, "Authentication request for user: {0} failed: {1}", new Object[]{username, failed});
+            LOGGER.log(FINER, "Authentication request for user: {0} failed: {1}", new Object[] { username, failed });
             return null;
         }
     }
@@ -71,8 +85,8 @@ public class BasicHeaderRealPasswordAuthenticator extends BasicHeaderAuthenticat
     private static final Logger LOGGER = Logger.getLogger(BasicHeaderRealPasswordAuthenticator.class.getName());
 
     /**
-     * Legacy property to disable the real password support.
-     * Now that this is an extension, {@link ExtensionFilter} is a better way to control this.
+     * Legacy property to disable the real password support. Now that this is an extension,
+     * {@link ExtensionFilter} is a better way to control this.
      */
     @SuppressFBWarnings(value = "MS_SHOULD_BE_FINAL", justification = "for script console")
     public static boolean DISABLE = SystemProperties.getBoolean("jenkins.security.ignoreBasicAuth");
